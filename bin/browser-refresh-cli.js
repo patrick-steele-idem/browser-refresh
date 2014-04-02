9259a8db6ed69bea2907cb6d90bae64f57dd270e
Patrick Steele-Idem <pnidem@gmail.com>
2014-04-01 23:22:52 -0600

    Initial commit

---
+++ bin/browser-refresh-cli.js
@@ -0,0 +1,20 @@
+var nodePath = require('path');
+
+var argv = process.argv.slice(2);
+var args;
+
+if (argv.length) {
+    args = argv.slice(1);
+    script = argv[0];
+} else {
+    var pkg = require(nodePath.join(process.cwd(), 'package.json'));
+    args = [];
+    script = pkg.main || 'index.js';
+}
+
+script = nodePath.resolve(process.cwd(), script);
+
+require('../lib/index').start({
+    script: script,
+    args: args
+});
